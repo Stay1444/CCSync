24c29fe1da236e704d6149cd3bba997dee1d6a34
Stay <stay@zaroz.cloud>
2022-11-30 15:30:59 +0100

    ComputerFinder created

---
+++ CCSync.Server/Utils/ComputerFinder.cs
@@ -0,0 +1,23 @@
+﻿using CCSync.Server.Services;
+
+namespace CCSync.Server.Utils;
+
+public static class ComputerFinder
+{
+    public static async Task<string?> FindAsync(string root, string authId)
+    {
+        foreach (var directory in Directory.GetDirectories(root))
+        {
+            var path = Path.Combine(root, directory, AuthWaiterService.AUTH_FILE_NAME);
+            if (File.Exists(path))
+            {
+                if ((await File.ReadAllTextAsync(path)).Replace("\n","") == authId)
+                {
+                    return path;
+                }
+            }
+        }
+
+        return null;
+    }
+}
