4e764f9543e2b0c0ecc2e6417039398a2e912a30
Stay <stay@zaroz.cloud>
2022-11-30 13:49:46 +0100

    Added ID field and example to yaml comment

--- CCSync.Server/Entities/World.cs
+++ CCSync.Server/Entities/World.cs
@@ -4,9 +4,11 @@ namespace CCSync.Server.Entities;
 
 public record World
 {
+    public string Id { get; set; }
+    
     [YamlMember(Description = "Friendly world name that gets shown to the clients")]
     public required string Name { get; init; }
     
-    [YamlMember(Description = "Absolute path to the world folder which contains the ComputerCraft files")]
+    [YamlMember(Description = "Absolute path to the world folder which contains the ComputerCraft computers files. ~/world/computercraft/computer/")]
     public required string Path { get; init; }
 }
