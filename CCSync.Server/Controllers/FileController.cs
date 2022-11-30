f4c453f3e5ceb1ccbe58c90eb9b5588b6c6c8738
Stay <stay@zaroz.cloud>
2022-11-30 15:06:52 +0100

    Created controller

---
+++ CCSync.Server/Controllers/FileController.cs
@@ -0,0 +1,18 @@
+﻿using CCSync.RPC;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace CCSync.Server.Controllers;
+
+public sealed class FileController : FileService.FileServiceBase
+{
+    public override Task<Empty> OnClientFileChanged(IAsyncStreamReader<FileChanged> requestStream, ServerCallContext context)
+    {
+        return base.OnClientFileChanged(requestStream, context);
+    }
+
+    public override Task OnServerFileChanged(Empty request, IServerStreamWriter<FileChanged> responseStream, ServerCallContext context)
+    {
+        return base.OnServerFileChanged(request, responseStream, context);
+    }
+}
