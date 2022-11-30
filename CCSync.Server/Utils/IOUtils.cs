17f158a5d6e5a512839a980c9ac7e9e016775611
Stay <stay@zaroz.cloud>
2022-11-30 11:05:24 +0100

    IOUtils class

---
+++ CCSync.Server/Utils/IOUtils.cs
@@ -0,0 +1,31 @@
+﻿namespace CCSync.Server.Utils;
+
+public static class IOUtils
+{
+    public static bool IsFileLocked(FileInfo file)
+    {
+        if (!file.Exists) return false;
+        
+        FileStream stream = null;
+
+        try
+        {
+            stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        }
+        catch (IOException)
+        {
+            //the file is unavailable because it is:
+            //still being written to
+            //or being processed by another thread
+            //or does not exist (has already been processed)
+            return true;
+        }
+        finally
+        {
+            stream?.Close();
+        }
+
+        //file is not locked
+        return false;
+    }
+}
