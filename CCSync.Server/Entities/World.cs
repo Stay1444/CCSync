05d2a236ab6cc4a3af6129538a37202e56fcdcf0
Stay <stay@zaroz.cloud>
2022-11-30 11:05:53 +0100

    World Entity

---
+++ CCSync.Server/Entities/World.cs
@@ -0,0 +1,12 @@
+﻿using YamlDotNet.Serialization;
+
+namespace CCSync.Server.Entities;
+
+public record World
+{
+    [YamlMember(Description = "Friendly world name that gets shown to the clients")]
+    public required string Name { get; init; }
+    
+    [YamlMember(Description = "Absolute path to the world folder which contains the ComputerCraft files")]
+    public required string Path { get; init; }
+}
