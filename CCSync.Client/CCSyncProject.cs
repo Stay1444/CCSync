08c04ce9e57c3748b08494c65a0b641fdaf43c0b
Stay <stay@zaroz.cloud>
2022-11-30 13:51:07 +0100

    CCSyncProject

---
+++ CCSync.Client/CCSyncProject.cs
@@ -0,0 +1,8 @@
+﻿namespace CCSync.Client;
+
+public record CCSyncProject
+{
+    public required Guid Auth { get; init; }
+    public required string World { get; init; }
+    public required string Origin { get; init; }
+}
