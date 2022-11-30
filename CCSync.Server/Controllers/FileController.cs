62367e227af93ad9568fe44c35d6e70ad7db5498
Stay <stay@zaroz.cloud>
2022-11-30 15:31:22 +0100

    Implemented FileController#OnServerFileChanged

--- CCSync.Server/Controllers/FileController.cs
+++ CCSync.Server/Controllers/FileController.cs
@@ -1,4 +1,7 @@
 ﻿using CCSync.RPC;
+using CCSync.Server.Services;
+using CCSync.Server.Utils;
+using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 
@@ -6,13 +9,66 @@ namespace CCSync.Server.Controllers;
 
 public sealed class FileController : FileService.FileServiceBase
 {
+    private WorldProvider _worldProvider;
+    private FileListenerService _fileListenerService;
+    
+    public FileController(WorldProvider worldProvider, FileListenerService fileListenerService)
+    {
+        _worldProvider = worldProvider;
+        _fileListenerService = fileListenerService;
+    }
+
     public override Task<Empty> OnClientFileChanged(IAsyncStreamReader<FileChanged> requestStream, ServerCallContext context)
     {
         return base.OnClientFileChanged(requestStream, context);
     }
 
-    public override Task OnServerFileChanged(Empty request, IServerStreamWriter<FileChanged> responseStream, ServerCallContext context)
+    public override async Task OnServerFileChanged(Empty request, IServerStreamWriter<FileChanged> responseStream, ServerCallContext context)
     {
-        return base.OnServerFileChanged(request, responseStream, context);
+        var world = _worldProvider.GetWorld(context.RequestHeaders.GetValue("WorldId") ?? "");
+        var authId = context.RequestHeaders.GetValue("AuthId");
+        
+        if (world is null)
+        {
+            context.Status = new Status(StatusCode.NotFound, "World not found");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(authId) || string.IsNullOrWhiteSpace(authId))
+        {
+            context.Status = new Status(StatusCode.Unauthenticated, "AuthId invalid");
+            return;
+        }
+
+        var pathToComputer = await ComputerFinder.FindAsync(world.Path, authId);
+
+        if (pathToComputer is null)
+        {
+            context.Status = new Status(StatusCode.Unauthenticated, "AuthId invalid");
+            return;
+        }
+
+        _fileListenerService.Start(pathToComputer);
+        
+        while (!context.CancellationToken.IsCancellationRequested)
+        {
+            var (oldPath, newPath, sendContent) = await _fileListenerService.Listen();
+
+            var msg = new FileChanged
+            {
+                OldPath = oldPath,
+                NewPath = newPath
+            };
+            
+            if (sendContent)
+            {
+                var fs = File.OpenRead(newPath);
+                msg.Contents = await ByteString.FromStreamAsync(fs);
+                fs.Close();
+                await fs.DisposeAsync();
+            }
+
+            await responseStream.WriteAsync(msg);
+        }
     }
 }
